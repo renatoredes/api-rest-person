00708b09b0fc3d7d7dc3ca68e587e2e340ca8e65
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2021-03-03 03:48:59 -0300

    Email

--- src/main/java/com/crud/personapi/entity/Person.java
+++ src/main/java/com/crud/personapi/entity/Person.java
@@ -50,4 +50,7 @@ public class Person {
 
     @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<Phone> phones;
+    
+    @OneToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    private List<Email> emails;
 }
--- src/test/java/com/crud/personapi/utils/PhoneUtils.java
+++ src/test/java/com/crud/personapi/utils/PhoneUtils.java
@@ -5,18 +5,19 @@ import com.crud.personapi.entity.Phone;
 import com.crud.personapi.enums.PhoneType;
 
 public class PhoneUtils {
-
+    
     private static final String PHONE_NUMBER = "1199999-9999";
     private static final PhoneType PHONE_TYPE = PhoneType.MOBILE;
     private static final long PHONE_ID = 1L;
 
+    
     public static PhoneDTO createFakeDTO() {
         return PhoneDTO.builder()
                 .number(PHONE_NUMBER)
                 .type(PHONE_TYPE)
                 .build();
     }
-
+    
     public static Phone createFakeEntity() {
         return Phone.builder()
                 .id(PHONE_ID)
